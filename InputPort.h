5a3338ca0fface48269611dc1697050d41c2ec4c
Buğra Gedik <bgedik@gmail.com>
2013-08-28 20:15:44 +0300

    Changes

--- InputPort.h
+++ InputPort.h
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+
+// InputPort: User visible interface
+// InputPortImph implements InputPort: System implementation of a port
+
 class InputPort : public Port
 {
 	private:
