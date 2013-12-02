422eec13daa3ad7ed6269c2c5741dfcde291ff27
Buğra Gedik <bgedik@gmail.com>
2013-12-02 11:59:27 +0200

    Updated readme

--- code/StreamWithCoroutines/lib/obj/streamc/runtime/PortsImpl.d
+++
@@ -1,6 +0,0 @@
-obj/streamc/runtime/PortsImpl.o: src/streamc/runtime/PortsImpl.cpp \
-  src/streamc/runtime/PortsImpl.h include/streamc/InputPort.h
-
-src/streamc/runtime/PortsImpl.h:
-
-include/streamc/InputPort.h:
