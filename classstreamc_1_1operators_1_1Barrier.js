79a044714652c5a311026e5389227664668c469b
Buğra Gedik <bgedik@gmail.com>
2013-12-02 17:07:49 +0200

    updated documentation

--- classstreamc_1_1operators_1_1Barrier.js
+++ classstreamc_1_1operators_1_1Barrier.js
@@ -1,5 +1,6 @@
 var classstreamc_1_1operators_1_1Barrier =
 [
     [ "Barrier", "classstreamc_1_1operators_1_1Barrier.html#a9a8598681f1561cdf90ad44c45342105", null ],
+    [ "Barrier", "classstreamc_1_1operators_1_1Barrier.html#a8bd7c7dedf1473e240692be0111a0d4b", null ],
     [ "process", "classstreamc_1_1operators_1_1Barrier.html#a700886fb9deb460e932d9c43bfa923f0", null ]
 ];
--- search/functions_62.js
+++ search/functions_62.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['barrier',['Barrier',['../classstreamc_1_1operators_1_1Barrier.html#a9a8598681f1561cdf90ad44c45342105',1,'streamc::operators::Barrier']]]
+  ['barrier',['Barrier',['../classstreamc_1_1operators_1_1Barrier.html#a9a8598681f1561cdf90ad44c45342105',1,'streamc::operators::Barrier::Barrier(std::string const &amp;name)'],['../classstreamc_1_1operators_1_1Barrier.html#a8bd7c7dedf1473e240692be0111a0d4b',1,'streamc::operators::Barrier::Barrier(std::string const &amp;name, int numInputs)']]]
 ];
