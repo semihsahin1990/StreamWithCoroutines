808a2c3d2f9c31bac5d305be009216fe3ef9d097
Semih Sahin <semihsahin1990@gmail.com>
2014-04-28 05:01:14 +0300

    round robin split & merge

---
+++ code/StreamWithCoroutines/core/src/streamc/operators/RoundRobinMerge.cpp
@@ -0,0 +1,32 @@
+#include "streamc/operators/RoundRobinMerge.h"
+#include "streamc/Logger.h"
+#include <queue>
+using namespace streamc;
+using namespace streamc::operators;
+using namespace std;
+
+RoundRobinMerge::RoundRobinMerge(std::string const & name)
+  : Operator(name, 1, 1)
+{}
+
+RoundRobinMerge::RoundRobinMerge(std::string const & name, int numInputs)
+  : Operator(name, numInputs, 1)
+{}
+
+void RoundRobinMerge::process(OperatorContext & context)
+{
+  size_t iPortNo = 0;
+  size_t nInputs = getNumberOfInputPorts();
+  OutputPort & oport = context.getOutputPort(0);
+
+  while(!context.isShutdownRequested()) {
+    bool closed = context.getInputPort(iPortNo).waitTuple();
+    if(closed)
+      break;
+    
+    Tuple & tuple = context.getInputPort(iPortNo).getFrontTuple();
+    oport.pushTuple(tuple);
+    context.getInputPort(iPortNo).popTuple();
+    iPortNo = (iPortNo+1) % nInputs;
+  }
+}
---
+++ code/StreamWithCoroutines/core/src/streamc/operators/RoundRobinSplit.cpp
@@ -0,0 +1,31 @@
+#include "streamc/operators/RoundRobinSplit.h"
+
+using namespace streamc;
+using namespace streamc::operators;
+using namespace std;
+
+RoundRobinSplit::RoundRobinSplit(std::string const & name)
+  : Operator(name, 1, 2)
+{}
+
+RoundRobinSplit::RoundRobinSplit(std::string const & name, int numOutputs)
+  : Operator(name, 1, numOutputs)
+{}
+
+void RoundRobinSplit::process(OperatorContext & context)
+{
+  InputPort & iport = context.getInputPort(0);
+  size_t nOutputs = getNumberOfOutputPorts();
+  size_t oPortNo = 0;
+
+  while (!context.isShutdownRequested()) {
+    bool closed = iport.waitTuple();
+    if(closed)
+      break;
+
+    Tuple & tuple = iport.getFrontTuple();
+    context.getOutputPort(oPortNo).pushTuple(tuple);
+    iport.popTuple();
+    oPortNo = (oPortNo+1) % nOutputs;
+  }
+}
